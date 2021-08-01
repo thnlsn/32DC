d117eff94c3dceaff8cca74dbdd7e521452694fc
Thomas Nelson <thnlsn@gmail.com>
2021-08-01 14:40:01 -0700

    added icons folder, general css additions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './css/style.css';
 
+import Card from './components/Card';
+
 function App() {
-  return <div className='App'>test</div>;
+  return (
+    <div className='App'>
+      <div className='cards'>
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const border = (identity) => ({ backgroundColor: 'blueviolet' });
+
 const background = (url) => ({
   backgroundImage: `url(${url})`,
   backgroundPosition: 'center',
@@ -8,13 +10,13 @@ const background = (url) => ({
 });
 
 const Card = ({
-  name,
-  cost = {},
-  identity = 'c',
+  name = 'Kozilek, the Great Distortion',
+  cost = '{8}{C}{C}',
+  identity = [],
   art = 'https://c1.scryfall.com/file/scryfall-cards/art_crop/front/f/0/f06fc6e0-b22c-40d3-bb53-d5ec400d921c.jpg?1562943286',
 }) => {
   return (
-    <div className='card-container'>
+    <div className='card' style={border(identity)}>
       <div className='image-container' style={background(art)}>
         <div className='image-container__cost'>{cost}</div>
         <div className='image-container__name'>{name}</div>
