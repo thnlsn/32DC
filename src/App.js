47d0bef6444f2ef5a26bc98b98d280eb817e64c2
Thomas Nelson <thnlsn@gmail.com>
2021-08-05 06:10:30 -0700

    installed axios, added some js files, imported to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+//* Dependencies
 import React from 'react';
 import './css/style.css';
 
+//* Scripts
+import api from './js/api';
+
+//* Components
 import Card from './components/Card';
 
 function App() {
   return (
     <div className='App'>
       <div className='cards'>
-        <Card />
+        <Card name={'name'} cost={'cost'} identity={'identity'} art={'art'} />
       </div>
     </div>
   );
