2cdada1c2a5d36c06d604f9d46cf43e271df6d72
Thomas Nelson <thnlsn@gmail.com>
2021-08-06 21:32:26 -0700

    search commander async call working

--- src/js/api.js
+++ src/js/api.js
@@ -1,3 +1,15 @@
 import axios from 'axios';
 
-console.log(axios);
+const queryString = 'https://api.scryfall.com/cards/search/?order=name&q=';
+
+// Get all commanders
+const searchCommander = async (identity, string, isValidCommander = true) => {
+  const endpoint = `${queryString}${string}%2C+id%3D${identity}${
+    isValidCommander ? '+is%3Acommander' : '+type%3Acreature'
+  }`;
+  console.log(endpoint);
+  const response = await axios.get(endpoint);
+  console.log(response);
+};
+
+searchCommander('br', 'a', false);
